d4f57ae15e9df810dbf13bc515bdec06b72fb642
Karolina Ryzka <119966673+KarolinaRyzka@users.noreply.github.com>
2025-10-13 14:56:14 +0000

    test fixtures

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -4,4 +4,8 @@ package edu.luc.cs.laufer.cs371.shapes
 enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
   case Location(x: Int, y: Int, shape: Shape)
-  // TODO add missing cases (see test fixtures)
+  
+  // TODO add missing cases (see test fixtures) //done
+  case Group(shapes: Shape*)
+  case Ellipse(width: Int, height: Int)
+
