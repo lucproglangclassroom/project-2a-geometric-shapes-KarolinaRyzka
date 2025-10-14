d127bb05ccfe48cc21b8cb6e7da3778150f144bd
Karolina Ryzka <119966673+KarolinaRyzka@users.noreply.github.com>
2025-10-14 21:29:49 +0000

    all tests passing

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -3,14 +3,30 @@ package edu.luc.cs.laufer.cs371.shapes
 /** data Shape = Rectangle(w, h) | Location(x, y, Shape) */
 enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
-    require(width > 0, s"rectangle width must be pos")
-    require(height > 0, s"rectangle height must be pos")
   case Location(x: Int, y: Int, shape: Shape)
   
   // TODO add missing cases (see test fixtures) //done
   case Group(shapes: Shape*)
-    require(shapes.nonEmpty, "group must have at least one shape")
   case Ellipse(width: Int, height: Int)
-    require(width > 0, s"ellipse width radius must be pos")
-    require(height > 0, s"ellipse height radius must be pos")
+
+//for shape constructor valididity
+object Shape:
+  
+  object Rectangle:
+    def apply(width: Int, height: Int): Shape.Rectangle =
+      require(width  > 0, s"rectangle width must be positive")
+      require(height > 0, s"rectangle height must be positive")
+      new Shape.Rectangle(width, height)
+
+  object Group:
+    def apply(shapes: Shape*): Shape.Group =
+      require(shapes.nonEmpty, "group must have at least one shape")
+      new Shape.Group(shapes*)
+
+
+  object Ellipse:
+    def apply(width: Int, height: Int): Shape.Ellipse =
+      require(width  > 0, s"ellipse rx must be positive")
+      require(height > 0, s"ellipse ry must be positive")
+      new Shape.Ellipse(width, height)
 
--- src/test/scala/TestBoundingBox.scala
+++ src/test/scala/TestBoundingBox.scala
@@ -45,3 +45,52 @@ class TestConstructors extends AnyFunSuite:
 
 end TestConstructors
 
+class TestSize extends AnyFunSuite:
+  
+  def testSize(description: String, s: Shape, expected: Int): Unit =
+    test(description):
+      assert(size(s) == expected)
+
+  testSize("simple ellipse", simpleEllipse, 1)
+  testSize("simple rectangle", simpleRectangle, 1)
+  testSize("simple location", simpleLocation, 1)
+  testSize("basic group", basicGroup, 2)
+  testSize("simple group", simpleGroup, 2)
+  testSize("complex group", complexGroup, 5)
+
+end TestSize
+
+
+class TestHeight extends AnyFunSuite:
+
+  def testHeight(description: String, s: Shape, expected: Int): Unit =
+    test(description):
+      assert(height(s) == expected)
+
+  testHeight("simple ellipse", simpleEllipse, 1)
+  testHeight("simple rectangle", simpleRectangle, 1)
+  testHeight("simple location", simpleLocation, 2)
+  testHeight("basic group", basicGroup, 2)
+  testHeight("simple group", simpleGroup, 3)
+  testHeight("complex group", complexGroup, 6)
+
+end TestHeight
+
+
+class TestScale extends AnyFunSuite:
+  def testScale(description: String, s: Shape, f: Int, x: Int, y: Int, width: Int, height: Int): Unit =
+    test(description):
+      val Location(u, v, Rectangle(w, h)) = (boundingBox(scale(s, f)): @unchecked)
+      assert(x == u)
+      assert(y == v)
+      assert(width == w)
+      assert(height == h)
+
+  testScale("scale simple ellipse by 2", simpleEllipse, 2, -100, -60, 200, 120)
+  testScale("scale simple rectangle by 3", simpleRectangle, 3, 0, 0, 240, 360)
+  testScale("scale simple location by 2", simpleLocation, 2, 140, 60, 160, 240)
+  testScale("scale basic group by 2", basicGroup, 2, -100, -60, 200, 140)
+  testScale("scale simple group by 2", simpleGroup, 2, 300, 140, 700, 560)
+  testScale("scale complex group by 2", complexGroup, 2, 60, 120, 940, 640)
+
+end TestScale
